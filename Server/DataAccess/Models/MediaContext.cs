923131261f28c35ba452a57c80d20dc9f376d0c2
Libor <libor.bicanovsky@gmail.com>
2020-12-03 22:09:16 +1300

    Setup backend

---
+++ Server/DataAccess/Models/MediaContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Models
+{
+    public class MediaContext : DbContext
+    {
+
+        public DbSet<Movie> Movies { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=FurionMediaServer.db");
+        }
+    }
+}
